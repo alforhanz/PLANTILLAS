6cf19a53aa02db2030fe2ddc438439537463c18a
Jose <alforhanz@gmail.com>
2024-04-28 11:19:30 -0500

    Subiendo Plantillas al Repositorio Remoto

---
+++ creaProyect.php
@@ -0,0 +1,18 @@
+<?php
+$projectName = "Enigma-1";
+
+if (!is_dir($projectName)) {
+    mkdir($projectName);
+    mkdir($projectName . '/app');
+    mkdir($projectName . '/app/models');
+    mkdir($projectName . '/app/controllers');
+    mkdir($projectName . '/app/views');
+    mkdir($projectName . '/public');
+    mkdir($projectName . '/public/css');
+    mkdir($projectName . '/public/js');
+    mkdir($projectName . '/public/img');
+    mkdir($projectName . '/config');
+    mkdir($projectName . '/sql');
+    mkdir($projectName . '/vendor');
+    mkdir($projectName . '/Clases');    
+}
